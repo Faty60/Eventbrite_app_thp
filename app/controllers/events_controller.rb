c4a1f46f29669e6e2d2e4b20c79820cebeea38c8
Faty60 <sidib.fatou2@gmail.com>
2021-05-20 04:02:18 +0200

    adding the creat event (view new show)

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,8 +1,43 @@
 class EventsController < ApplicationController
+
+  before_action :authenticate_user!, except: [:index, :show] 
+
   def index
     @events = Event.all
   end
   
   def new
+    @event = Event.new
+  end
+
+  def create
+    @start_date = DateTime.strptime(params[:start_date],"%Y-%m-%d")
+    @event = Event.create(
+    start_date: @start_date,
+    duration: params[:duration],
+    title: params[:title],
+    description: params[:description],
+    price: params[:price],
+    location: params[:location],
+    admin: current_user
+    )
+
+    if @event.save
+      redirect_to event_path(@event.id), alert: "Votre évènement est bien enregistrée !"
+    else
+      flash.now[:alert] = "Oups il y a eu une erreur, veuillez réessayer"
+      render :new
+    end
+  end
+
+  def show
+    @event = Event.find(params[:id])
+
+  end
+
+  private
+
+  def event_params
+    params.permit(:start_date, :duration, :title, :description, :price, :location)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,16 +17,45 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to @user, notice: 'User was successfully created.'
+      redirect_to @user, notice: 'Votre compte est bien enregistré'
     else
-      flash[:danger] = 'Something went wrong - please check your input'
+      flash[:danger] = 'Oups il y a une erreur, veuillez réessayer'
       render :new
     end
   end
 
+  def edit
+    @user = user_find
+  end
+
   def update
+    @user = user_find
+    if @user.update(post_params)
+      redirect_to root_path, notice: 'Modifications bien enregistrée !'
+    else
+      flash[danger] = "Oups il y a eu une erreur, veuillez réessayer"
+      render :edit
+    end
   end
 
   def destroy
+  end
+private
+    # Use callbacks to share common setup or constraints between actions.
+   def set_user
+     @user = User.find(params[:id])
+   end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+   def user_params
+     params.require(:user).permit(:email, :password, :password_confirmation, :first_name, :last_name, :description)
+   end
+
+  def user_find
+    @user = User.find(params[:id])
+  end
+
+  def post_params
+    post_params = params.permit(:first_name, :last_name, :description)
   end
 end
